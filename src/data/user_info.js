107baf335940c0b5d8212ad7440f820dfa83b97e
pallavi123426 <pallupallavi90779@gmail.com>
2025-11-04 13:10:24 +0530

    index

--- src/data/user_info.js
+++ src/data/user_info.js
@@ -2,10 +2,10 @@ const info = {
     main: {
         name: "Pallavi B S",
         description: "Hello i am Pallavi BS a Computer Science Engineering passionate about Artificial Inteligence,Web Development,and Cybersecurity.I lovecreating smart and efficient solution from building AI based systems to designing responsive websites that provide smooth user experiences, I've worked on projects like Face Recognition Based Attendance System Using CNN,which deepened my interest in machine learning and image processing.During my interships in Cybersecurity and Java Programming at System Tron, I gained hands-on experience with real_world applications and team collaboration.I'm currently exporing full-stack developement and AI integration in web application, and I'm always excited to lear new technologies that make a difference. Outside of coding, I enjoy problem-solving challengs, exploring design ideas, and improving my technical skills through self-learning and online projects.",
-        role: "Java DEVELOPER",
-        photo: "../photo.jpg",
+        role: "JAVA DEVELOPER",
+        photo: "../dp.jpg",
         email: "pallupallavi90779@gmail.com",
-        //tracking_id: "G-BNTTBGHXSC"
+        
     },
     resume: {
         link: "../pallavi.pdf" // Replace with your actual resume URL
@@ -13,19 +13,19 @@ const info = {
     
 
     socials: {
-       // twitter: "https://x.com/srinivaspavan73",
-		github: "https://github.com/pallavi123426/pallu",
-		linkedin: "https://www.linkedin.com/in/PallaviPallu",
+		github: "https://github.com/Pallavi123426",
+		linkedin: "https://www.linkedin.com/in/Pallavi B S",
 		instagram: "https://instagram.com/9247_pallavi",
-		//facebook: "https://www.facebook.com/",
         telegram: "https://telegram.me/Pallavi_Pallu"
     },
     experience: [
         {
             company: "Bescom",
             role: "Cyber Security",
-            description: " A cybersecurity intership involves assisting with security tasks like monitoring alers, identifying vulnerabilities,responding to incidents,and conducting research on threas under the guidance of experienced professionals.",
+            description: "A cybersecurity intership involves assisting with security tasks like monitoring alers, identifying vulnerabilities,responding to incidents,and conducting research on threas under the guidance of experienced professionals.",
             period: "July 2023- 2024"
+
+            
         },
         {
             company: "Bescom",
@@ -34,38 +34,8 @@ const info = {
             // description: "Worked as an Artificial Intelligence Intern, receiving a stipend for developing various AI tools tailored to company requirements using Python and JavaScript. This role enhanced my technical skills and allowed me to contribute effectively to the team’s objectives.",
             period: "July 2023 - February 2024"
         },
-        {
-            //company: "Entersoft",
-            //role: "Smart Contracts Aduitor",
-            //description:"Contributed to the security and reliability of blockchain projects by auditing smart contracts and developing comprehensive test cases. Enhanced the security posture of multiple projects by identifying vulnerabilities and ensuring compliance with best practices. Gained hands-on experience in blockchain security, improving the robustness of smart contracts and reducing potential risks through meticulous analysis and testing.",
-            // description: "Contributed to the security of blockchain projects at Entersoft by auditing smart contracts and writing comprehensive test cases. Acquired valuable hands-on experience in a vital, emerging field of cybersecurity, enhancing project security and reliability through meticulous analysis and testing.",
-            //period: "November 2023- December 2023"
-        },
         
-        {
-            //company: "EduSkills",
-            //role: "AWS Clould Intern",
-            //description: "Engaged in a cloud computing internship using AWS, mastering the deployment, scaling, and management of cloud applications. Gained expertise in AWS services like EC2, S3, and RDS, applying best practices in cloud architecture and security to simulated business environments.",
-            //period: "Sepetember 2023 - November 2023"
-        },
-        {
-            //company: "EduSkills",
-            //role: "AWS Cloud Intern",
-            //description: "Advanced my skills in AWS Cloud technologies through a comprehensive virtual internship, focusing on developing scalable cloud solutions using Lambda, DynamoDB, and Elastic Beanstalk. Managed projects that emphasized workflow automation and robust security, enhancing cloud infrastructure resiliency.",
-            //period: "June 2023 - November 2023"
-        },                
-        {
-            //company: "EduSkills",
-            //role: "Data Analyst Inten",
-            //description: "Leveraged AWS technologies in a virtual data analytics internship, analyzing large datasets to drive insights and improve decision-making. Utilized machine learning models and AWS QuickSight for effective data visualization and reporting in collaborative projects.",
-            //period: "May 2023 - July 2023"
-        },        
-        {
-            //company: "YHills Edutech Private Limited",
-            //role: "Python Developer Intern",
-            //description: "As a Python Developer Intern, I developed Python modules for machine learning algorithms and aggregated unstructured data from various sources to lay the groundwork for a new product. Additionally, I gained hands-on experience with data manipulation and analysis using NumPy and Pandas.",
-            //period: "October 2022 - December 2022"
-        },                
+            
         // More experiences...
     ],
 
@@ -89,6 +59,7 @@ const info = {
 			title: "Face Recognition Based Attendance System Using CNN",
 			description: "Faceial recongnition is a biometric technology that identifiesnor verrifies a person's identify by analyzing and measuring unique feacture from a digital image or video.",
 			technologies: "https://skillicons.dev/icons?i=react,opencv,flask,tensorflow&perline=5&theme=",
+			technologies: "https://skillicons.dev/icons?i=react,opencv,flask,tensorflow&perline=5&theme=",
 			github: "https://github.com/Pallavi123426/Secure-Access",
             // link: "https://letterdigitshape-classification.netlify.app/"
 		},
@@ -110,32 +81,32 @@ const info = {
 		// },
 
 		{
-			title: "Cyber Security",
+			title: "Cyber SEcurity",
 			description: "creating a system or tool to protect against cyber threats, such as building an intrusion detection system to monitor network traffic.",
 			technologies: "https://skillicons.dev/icons?i=react,firebase,0,0,0&perline=5&theme=",
 			github: "https://github.com/Pallavi123426/Weaver-s-Loom",
-            //link: "https://weavers-loom.netlify.app/"
+            link: "https://weavers-loom.netlify.app/"
 		},
     ],
 
     education: [
         {
             school: "Kalpataru Instite of Technology",
-            degree: "BE in computer science",
+            degree: "BE in Computer Science",
             // year: "2023 - Present",
-            image: "kit.jpg"
+            image: "kitlogo.jpg"
         },
         // {
-        //     school: "Govt Womens polytechnic college ",
-        //     degree: "Diploma",
-        //     year: "2020 - 2023",
-        //     image: "gvt.png"
+        //     school: "sri gowthami junior college ",
+        //     degree: "INTERMEDIATE",
+        //     year: "2019 - 2021",
+        //     image: "sgjclogo.png"
         // },
     ],
 
     contact: {
         title: "Reach Out and Connect",
-        description: "Thank you for your interest in connecting with me. I welcome your feedback, questions, and suggestions. If you have a specific inquiry or comment, please feel free to email me directly at pallupallavi90779@gmail.com. I strive to respond to all messages within 24 hours, but please allow a bit more time during busy periods. Alternatively, if you prefer to connect on social media, you can find me using the links provided below.",
+        description: "Thank you for your interest in connecting with me. I welcome your feedback, questions, and suggestions. If you have a specific inquiry or comment, please feel free to email me directly at bitrasrinivaspavan@gmail.com. I strive to respond to all messages within 24 hours, but please allow a bit more time during busy periods. Alternatively, if you prefer to connect on social media, you can find me using the links provided below.",
     },
 
     footer: "© 2025 Pallavi B S. All Rights Reserved"
